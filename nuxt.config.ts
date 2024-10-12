19d942ad019471ce7bf56c1665886e60ea535f45
Asya <unkiplum@gmail.com>
2024-10-12 15:04:08 +0200

    feat: add user addition form

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [{ rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" }],
+    },
+  },
+});
